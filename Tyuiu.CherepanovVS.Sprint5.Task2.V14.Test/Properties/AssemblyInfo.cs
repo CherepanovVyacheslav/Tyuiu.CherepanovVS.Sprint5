0e93b3b2adbda22497050e20f414102f744075ad
Tyuiu.CherepanovVS@outlook.com <Tyuiu.CherepanovVS@outlook.com>
2023-11-17 23:29:53 +0500

    Task2

---
+++ Tyuiu.CherepanovVS.Sprint5.Task2.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.CherepanovVS.Sprint5.Task2.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.CherepanovVS.Sprint5.Task2.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f81633c-32dc-41d6-83c9-51db5a89cf82")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.CherepanovVS.Sprint5.Task2.V14/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.CherepanovVS.Sprint5.Task2.V14.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint5.Task2.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Черепанов В.С. | ПКТб-23-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*Спринт 5                                                                  *");
+            Console.WriteLine("*Тема: Класс File. Запись структурированных данных в текстовый файл        *");
+            Console.WriteLine("*Задание #2                                                                *");
+            Console.WriteLine("*Вариант #14                                                               *");
+            Console.WriteLine("*Выполнил: Черепанов Вячеслав Сергеевич| ПКТб-23-1                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный         *");
+            Console.WriteLine("* значениями с клавиатуры. Заменить положительные элементы массива на 1,   *");
+            Console.WriteLine("* отрицательные на 0. Результат сохранить в файл OutPutFileTask2.csv и     *");
+            Console.WriteLine("* вывести на консоль.                                                      *");
+            Console.WriteLine("* - 3; -6; -3                                                              *");
+            Console.WriteLine("*   6;  8;  3                                                              *");
+            Console.WriteLine("* - 6; -5;  5                                                              *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Введите количество строк в массиве:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в массиве:");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[,] mtrx = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine($"Введите {j},{i} элемент массива");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            string res = ds.SaveToFileTextData(mtrx);
+            Console.WriteLine("Файл:" + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
